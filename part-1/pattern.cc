bfded447bc4390185bbfd212e1ea1d904a4b49a2
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-10-11 11:06:36 -0700

    made a header and got some sort of pattern going

--- part-1/pattern.cc
+++ part-1/pattern.cc
@@ -1,13 +1,41 @@
-
-// TODO: Insert your header.
+// Alex Labitigan
+// CPSC 120-12
+// 2022-10-10
+// alexlabitigan@csu.fullerton.edu
+// @auhlz
+//
+// Lab 06-01
+// Partners: @farrah-e
+//
+// Makes a pattern
+//
 
 #include <iostream>
 
 // TODO: Declare a const int named kCounterMax and initialize it to 22.
 
+int const kCounterMax{22};
+
+
 int main(int argc, char const *argv[]) {
   // TODO: Write an outer loop which starts from 0 and goes up to the counter
   // max.
+for(int i = 0; i <= kCounterMax; i++) 
+{
+    for(int j = 0; j <= kCounterMax-i; j++)
+    {
+      std::cout << i + j << " ";
+    }
+      std::cout << "\n";
+      // //for()
+      // {
+
+
+      // }
+
+}
+
+
 
   // TODO: Write an inner loop which starts from the current line number and
   // counts down to zero. Make sure that this loop is inside the outer loop.
